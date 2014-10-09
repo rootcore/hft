4577aaf0bfb16057b9820c97af84543a31f4c302
Andre <admin@howtofixtutorial.com>
2014-10-09 16:48:49 +0700

    Create sendsms.php

---
+++ sendsms.php
@@ -0,0 +1,36 @@
+<html>
+  <head>
+// source : http://www.howtofixtutorial.com/en/2012-10-12-07-20-28/smart-search/2014-10-09-09-27-41/stop-start-check-saldo-on-gammu
+// Created by : andr3
+// E-mail : andre@howtofixtutorial.com
+// Website : http://www.howtofixtutorial.com
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>::Input Send SMS::</title>
+  </head>
+  <body background="background4.png">
+  <form action="send.php"  method="post" enctype="multipart/form-data"  name="FMHS">
+  <table  width="452" border="0" align="center"  cellpadding="0" cellspacing="1"  bgcolor="#1605c0">
+  <tr>
+  <table width="50%" align="center" valign="top" border="0">
+        </th></tr>
+<tr>
+
+<td><table>
+  <tr>
+  <tr><td  width=""><b>Recipient Number</b></td><td width="11">:</td>
+  <td  width="237"><input required="required" name="noTelp" type="text"  id="noTelp" size="30" maxlength="30"></td>
+  </tr>
+        <td><b> Input SMS</b></td>
+    <td> : </td>
+    <td colspan="7"> <textarea required="required" name="message" cols="41" rows="7"></textarea </td>
+</tr>
+  <tr><td  colspan="3" align="center"><input  name="fok" type="submit" id="fok"  value="Send SMS">
+  <input  name="fulang" type="reset" id="fulang"  value="Clear Form">
+
+  </tr>
+  </table></td>
+  </tr>
+  </table>
+  </form>
+  </body>
+</html>
